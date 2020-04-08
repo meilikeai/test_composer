5fa3fa032310056e1d227ee27faf8066354669ea
meilikeai0123 <122528759@qq.com>
2020-04-08 12:16:10 +0800

    修改

---
+++ src/SayHello.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace functions;
+/**
+ * notes: SayHello
+ * author: peipei
+ * date: 2020/4/8 12:09
+ */
+class SayHello
+{
+    public static function world()
+    {
+        return 'hello world';
+    }
+}
