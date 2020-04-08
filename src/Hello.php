ecdc087299869b48b675fad6b28396352c19770c
meilikeai0123 <122528759@qq.com>
2020-04-08 13:38:35 +0800

    修改

---
+++ src/Hello.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace meilikeai;
+/**
+ * notes: Hello
+ * author: peipei
+ * date: 2020/4/8 13:34
+ */
+class Hello
+{
+    static function worls()
+    {
+        return '111';
+    }
+}
